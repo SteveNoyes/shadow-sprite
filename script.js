9d60890d1d4896dc951c8ef27bcac307dae321d5
SteveNoyes <stevenmnoyes.af@gmail.com>
2022-08-26 15:34:02 -0400

    sprite list

--- script.js
+++ script.js
@@ -1,25 +1,95 @@
-// basic sprite animation
-// grab canvas from html
+// create playerState for initial animation
+let playerState = 'idle';
+// dropdown menu
+const dropdown = document.getElementById('animations');
+dropdown.addEventListener('change', function(e){
+	playerState = e.target.value;
+})
+// grab canvas from html file, apply 2d and give width and height
 const canvas = document.getElementById('canvas0');
-// give canvas 2d features
 const ctx = canvas.getContext('2d');
-// define cavas width and height
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
-// create new Image and assign it sprite sheet
+// add sprite sheet to new image variable
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
-// set single sprite width, width of sheet / # of sprites
+// set sprite width by dividing full width of sheet by how many rows of sprites
 const spriteWidth = 575;
-// set single sprite height, height of sheet / # of sprites
+// set sprite height by dividing full height of sheet by how many columns of sprites
 const spriteHeight = 523;
 
-// function to animate the sprite based on which
-function animate() {
-	// s = image source, d = image destination
-	// ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+let gameFrame = 0;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+	{
+		name: 'idle',
+		frames: 7,
+	},
+	{
+		name: 'jump',
+		frames: 7,
+	},
+	{
+		name: 'fall',
+		frames: 7,
+	},
+	{
+		name: 'run',
+		frames: 9,
+	},
+	{
+		name: 'dizzy',
+		frames: 11,
+	},
+	{
+		name: 'sit',
+		frames: 5,
+	},
+	{
+		name: 'roll',
+		frames: 7,
+	},
+	{
+		name: 'bite',
+		frames: 7,
+	},
+	{
+		name: 'ko',
+		frames: 12,
+	},
+	{
+		name: 'gethit',
+		frames: 4,
+	}
+];
 
-	ctx.drawImage(playerImage, 0, 0, 200, 200, 0, 0,  );
+// state of each animation matched to loc object
+animationStates.forEach((state, index) => {
+	let frames = {
+		loc: [],
+	}
+	for (let i = 0; i < state.frames; i++){
+		let positionX = i * spriteWidth;
+		let positionY = index * spriteHeight;
+		frames.loc.push({x: positionX, y: positionY});
+	}
+	spriteAnimations[state.name] = frames;
+});
+
+function animate() {
+	// reset canvas
+	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+	// this staggers the frames by a fifth
+	let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
+	let frameX = spriteWidth * position;
+	let frameY = spriteAnimations[playerState].loc[position].y;
+	// this takes 3, 5 or 9 parameters
+	// source image, source coordinates, desination coordinates
+	// anything.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
+	ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
+	gameFrame++;
 	requestAnimationFrame(animate);
 };
 animate();
