9198fd6b3f3bb965f7a7940bd92d63d2608eab2f
SteveNoyes <stevenmnoyes.af@gmail.com>
2022-10-19 20:46:04 -0400

    single set

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // create playerState for initial animation
 let playerState = 'idle';
-// dropdown menu
+// dropdown 
+// menu
 const dropdown = document.getElementById('animations');
 // update playerState variable on change of dropdown menu
-// to the selected item
 dropdown.addEventListener('change', function(e){
 	playerState = e.target.value;
 })
@@ -12,7 +12,8 @@ const canvas = document.getElementById('canvas0');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
-// add sprite sheet to new image variable
+// add sprite sheet to 
+// new image variable
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
 // set sprite width by dividing full width of sheet by how many rows of sprites
